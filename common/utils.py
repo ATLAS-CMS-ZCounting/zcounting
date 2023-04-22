c003dbb685fa43da0a354c7e12e4b5befcb6c77c
dwalter <dwalter@cern.ch>
2023-04-23 00:35:14 +0200

    Cumulative ratio from Z boson rate

--- common/utils.py
+++ common/utils.py
@@ -2,6 +2,11 @@ import matplotlib as mpl
 from datetime import datetime
 
 def to_mpl_time(timestring, atlas=False):
+    datetime_object = to_datetime(timestring, atlas)
+   
+    return mpl.dates.date2num(datetime_object)
+
+def to_datetime(timestring, atlas=False):
     # our agreed format has "YY/MM/DD HH:MM:SS"
     if atlas:
         formatstr = ' %y/%m/%d %H:%M:%S'
@@ -12,5 +17,5 @@ def to_mpl_time(timestring, atlas=False):
         datetime_object = datetime.strptime(timestring, formatstr)
     else:
         datetime_object = timestring
-   
-    return mpl.dates.date2num(datetime_object)
+    
+    return datetime_object
--- scripts/plot_fill.py
+++ scripts/plot_fill.py
@@ -16,7 +16,9 @@ parser.add_argument("-f", "--fills", default=[], type=int, nargs="*",
 parser.add_argument("--no-ratio", action="store_true",
                     help="Make no ratio")
 parser.add_argument("--fmts", default=["png", ], type=int, nargs="+",
-                    help="List of formats to store the plots")                    
+                    help="List of formats to store the plots")
+parser.add_argument("--no-ref", default=["png", ], action="store_true",
+                    help="Don't show reference lumi")                    
 args = parser.parse_args()
 log = logging.setup_logger(__file__, args.verbose)
 
@@ -24,8 +26,8 @@ colors, textsize, labelsize, markersize = plotting.set_matplotlib_style()
 
 color_cms = colors[0]
 color_atlas = colors[1]
-marker_cms = "^"
-marker_atlas = "v"
+marker_cms = "o"
+marker_atlas = "o"
 
 
 with open(args.atlas_csv, "r") as ifile:
@@ -45,14 +47,20 @@ if not os.path.isdir(args.outputDir):
 
 def convert_time(df, atlas=False):
     # convert time
-    df['timeDown'] = df['beginTime'].apply(lambda x: utils.to_mpl_time(x, atlas))
-    df['timeUp'] = df['endTime'].apply(lambda x: utils.to_mpl_time(x, atlas))
+    df['timeDown'] = df['beginTime'].apply(lambda x: utils.to_datetime(x, atlas))
+    df['timeUp'] = df['endTime'].apply(lambda x: utils.to_datetime(x, atlas))
 
     # center of each time slice
-    df['time'] = df['timeDown'] + (df['timeUp'] - df['timeDown'])/2
+    df['timewindow'] = df['timeUp'] - df['timeDown']
+    df['time'] = df['timeDown'] + df['timewindow']/2
+
+    df["time"] = df["time"].apply(lambda x: utils.to_mpl_time(x, atlas))
+    df["timeUp"] = df["timeUp"].apply(lambda x: utils.to_mpl_time(x, atlas))
+    df["timeDown"] = df["timeDown"].apply(lambda x: utils.to_mpl_time(x, atlas))
 
 convert_time(df_atlas, atlas=True) 
 convert_time(df_cms) 
+
 
 for fill in fills:
     if fill not in df_cms["fill"].values:
@@ -88,11 +96,14 @@ for fill in fills:
     x_atlas, xUp_atlas, xDown_atlas = get_x(dfill_atlas)
 
     # x axis range
+    time_cms = max(x_cms) - min(x_cms)
+    time_atlas = max(x_atlas) - min(x_atlas)
+
     xMin = min(min(x_cms-xDown_cms), min(x_atlas-xDown_atlas))
     xMax = max(max(x_cms+xUp_cms), max(x_atlas+xUp_atlas))
     xRange = xMax - xMin
-    xMin = xMin - xRange * 0.025
-    xMax = xMax + xRange * 0.025
+    xMin = xMin - xRange * 0.015
+    xMax = xMax + xRange * 0.015
     xRange = xMax - xMin
 
     def get_y(df):
@@ -106,9 +117,12 @@ for fill in fills:
     y_cms, yErr_cms = get_y(dfill_cms)
     y_atlas, yErr_atlas = get_y(dfill_atlas)
 
+    y_lumi_cms = dfill_cms["instDelLumi"] * 650.
+    y_lumi_atlas = dfill_atlas["instDelLumi"] * 650.
+
     ticksteps = 1 + xRange // 8 
 
-    xTicks = np.arange(0, int(xMax)+ticksteps, ticksteps)
+    xTicks = np.arange(0, int(xMax), ticksteps)
     
     # make plot of Z boson rate as function of LHC fill time
     plt.clf()
@@ -125,13 +139,28 @@ for fill in fills:
     ax1.set_title(f"Fill {fill}")
     ax1.set_ylabel("Z boson rate [Hz]")
     
-    ax1.errorbar(x_cms, y_cms, xerr=(xDown_cms, xUp_cms), yerr=yErr_cms, label="CMS", color=color_cms, marker=marker_cms,
+    # ax1.errorbar(x_cms, y_cms, xerr=(xDown_cms, xUp_cms), yerr=yErr_cms, label="CMS", color=color_cms, marker=marker_cms, mfc='none',
+    #     linestyle='', zorder=0)
+
+    # ax1.errorbar(x_atlas, y_atlas, xerr=(xDown_atlas, xUp_atlas), yerr=yErr_atlas, label="ATLAS", color=color_atlas, marker=marker_atlas,
+    #     linestyle='', zorder=0)
+
+    if not args.no_ref:
+        ax1.errorbar(x_cms, y_lumi_cms, xerr=(xDown_cms, xUp_cms), label="CMS L", color=color_cms, 
+            linestyle='', zorder=0)
+
+        ax1.errorbar(x_atlas, y_lumi_atlas, xerr=(xDown_atlas, xUp_atlas), label="ATLAS L", color=color_atlas, 
+            linestyle='', zorder=0)
+
+
+    ax1.plot(x_cms, y_cms,label="CMS Z", color=color_cms, marker=marker_cms, mfc='none',
         linestyle='', zorder=0)
 
-    ax1.errorbar(x_atlas, y_atlas, xerr=(xDown_atlas, xUp_atlas), yerr=yErr_atlas, label="ATLAS", color=color_atlas, marker=marker_atlas,
+    ax1.plot(x_atlas, y_atlas, label="ATLAS Z", color=color_atlas, marker=marker_atlas, mfc='none',
         linestyle='', zorder=0)
 
-    leg = ax1.legend(loc="lower left")
+
+    leg = ax1.legend(loc="upper right", ncol=2)
 
     yMin = min(min(y_cms-yErr_cms),min(y_atlas-yErr_atlas))
     yMax = max(max(y_cms+yErr_cms),max(y_atlas+yErr_atlas))
@@ -146,11 +175,33 @@ for fill in fills:
         ax2.set_xlabel("LHC runtime")
         ax2.set_ylabel("ATLAS / CMS")
 
-        #TODO
-            
+        nPoints = 1000
+        xx = np.linspace(xMin, xMax, nPoints)
+        yy_atlas = np.array([y_atlas[x_atlas < x][-1] if any(x_atlas < x) else 0 for x in xx])
+        yy_cms = np.array([y_cms[x_cms < x][-1] if any(x_cms < x) else 0 for x in xx])
+
+        ratio = np.nan_to_num(yy_atlas/yy_cms, nan=1, posinf=1, neginf=1)
+
+        if not args.no_ref:
+            ax2.errorbar(x_cms, y_cms/y_lumi_cms, xerr=(xDown_cms, xUp_cms), label="Z", color=color_cms, 
+                linestyle='', zorder=0)
+
+            ax2.errorbar(x_atlas, y_atlas/y_lumi_atlas, xerr=(xDown_atlas, xUp_atlas), label="A", color=color_atlas, 
+                linestyle='', zorder=0)
+
+        # ax2.plot(xx, ratio, color="black", marker=None, linestyle='--', zorder=0)       
+        
+        ratio_cumsum = np.nan_to_num(np.cumsum(yy_atlas)/np.cumsum(yy_cms), nan=1, posinf=1, neginf=1)
+
+        ax2.plot(xx, ratio_cumsum, color="black", marker=None,
+            linestyle='-', zorder=0)
+
+        ratio = round(ratio_cumsum[-1],3)
+        ax2.text(0.4, 0.6 if ratio<1 else 0.2, "Integrated Z ratio: "+str(ratio), verticalalignment='bottom', transform=ax2.transAxes)
+
         ax2.plot(np.array([xMin, xMax]), np.array([1.0, 1.0]), color="black",linestyle="--", linewidth=1)
         
-        ax2.set_ylim([0.961,1.039])
+        ax2.set_ylim([0.71,1.29])
         ax2.set_xlim([xMin, xMax])
         ax2.set_xticks(xTicks)
 
@@ -160,12 +211,9 @@ for fill in fills:
     else:
         ax1.set_xlabel("LHC runtime")
 
-
-
     for fmt in args.fmts:
         plt.savefig(args.outputDir+f"/fill_{fill}.{fmt}")
     plt.close()
-
 
     # make plot of cumulative Z boson rate as function of LHC fill time
     def get_y(df):
@@ -177,9 +225,17 @@ for fill in fills:
     y_cms, yErr_cms = get_y(dfill_cms)
     y_atlas, yErr_atlas = get_y(dfill_atlas)
 
+    y_lumi_cms = dfill_cms["delLumi"].cumsum().values* 650.
+    y_lumi_atlas = dfill_atlas["delLumi"].cumsum().values * 650.
+
     plt.clf()
     fig = plt.figure()
-    if not args.no_ratio:
+    if not args.no_ratio and not args.no_ref:
+        gs = gridspec.GridSpec(3, 1, height_ratios=[2, 1, 1])
+        ax1 = plt.subplot(gs[0])
+        ax2 = plt.subplot(gs[1])
+        ax3 = plt.subplot(gs[2])
+    elif not args.no_ratio:
         gs = gridspec.GridSpec(2, 1, height_ratios=[2, 1])
         ax1 = plt.subplot(gs[0])
         ax2 = plt.subplot(gs[1])
@@ -191,13 +247,26 @@ for fill in fills:
     ax1.set_title(f"Fill {fill}")
     ax1.set_ylabel("Number of Z bosons")
     
-    ax1.errorbar(x_cms, y_cms, xerr=(xDown_cms, xUp_cms), yerr=yErr_cms, label="CMS", color=color_cms, marker=marker_cms,
+    # ax1.errorbar(x_cms, y_cms, xerr=(xDown_cms, xUp_cms), yerr=yErr_cms, label="CMS", color=color_cms, marker=marker_cms,
+    #     linestyle='', zorder=0)
+
+    # ax1.errorbar(x_atlas, y_atlas, xerr=(xDown_atlas, xUp_atlas), yerr=yErr_atlas, label="ATLAS", color=color_atlas, marker=marker_atlas,
+    #     linestyle='', zorder=0)
+
+    if not args.no_ref:
+        ax1.errorbar(x_cms, y_lumi_cms, xerr=(xDown_cms, xUp_cms), label="CMS L", color=color_cms, 
+            linestyle='', zorder=0)
+
+        ax1.errorbar(x_atlas, y_lumi_atlas, xerr=(xDown_atlas, xUp_atlas), label="ATLAS L", color=color_atlas, 
+            linestyle='', zorder=0)
+
+    ax1.plot(x_cms, y_cms,label="CMS Z", color=color_cms, marker=marker_cms, mfc='none',
         linestyle='', zorder=0)
 
-    ax1.errorbar(x_atlas, y_atlas, xerr=(xDown_atlas, xUp_atlas), yerr=yErr_atlas, label="ATLAS", color=color_atlas, marker=marker_atlas,
+    ax1.plot(x_atlas, y_atlas, label="ATLAS Z", color=color_atlas, marker=marker_atlas, mfc='none',
         linestyle='', zorder=0)
 
-    leg = ax1.legend(loc="lower right")
+    leg = ax1.legend(loc="lower right", ncol=2)
 
     yMin = min(min(y_cms-yErr_cms),min(y_atlas-yErr_atlas))
     yMax = max(max(y_cms+yErr_cms),max(y_atlas+yErr_atlas))
@@ -210,11 +279,12 @@ for fill in fills:
     
     if not args.no_ratio:
         ax1.xaxis.set_major_locator(ticker.NullLocator())
-        ax2.set_xlabel("LHC runtime")
-        ax2.set_ylabel("ATLAS / CMS")
 
-        # cumulative ratio
-        xx = np.linspace(xMin, xMax, 100)
+        # cumulative Z ratio
+        ax2.set_ylabel("A / C")
+
+        nPoints = 1000
+        xx = np.linspace(xMin, xMax, nPoints)
         yy_atlas = np.array([y_atlas[x_atlas < x][-1] if any(x_atlas < x) else 1 for x in xx])
         yy_cms = np.array([y_cms[x_cms < x][-1] if any(x_cms < x) else 1 for x in xx])
 
@@ -225,15 +295,41 @@ for fill in fills:
 
         ax2.plot(np.array([xMin, xMax]), np.array([1.0, 1.0]), color="black", linestyle="--", linewidth=1)
         
-        ax2.text(0.5, 0.7 if ratio<1 else 0.2, "Integrated Z ratio: "+str(ratio), verticalalignment='bottom', transform=ax2.transAxes)
+        ax2.text(0.4, 0.6 if ratio<1 else 0.2, "Integrated Z ratio: "+str(ratio), verticalalignment='bottom', transform=ax2.transAxes)
 
-        ax2.set_ylim([0.8,1.2])
+        ax2.set_ylim([0.81,1.19])
         ax2.set_xlim([xMin, xMax])
         ax2.set_xticks(xTicks)
+
+        if not args.no_ref:
+            ax2.xaxis.set_major_locator(ticker.NullLocator())
     
+            # cumulative lumi ratio
+            ax3.set_xlabel("LHC runtime")
+            ax3.set_ylabel("A / C")
+
+            yy_lumi_atlas = np.array([y_lumi_atlas[x_atlas < x][-1] if any(x_atlas < x) else 1 for x in xx])
+            yy_lumi_cms = np.array([y_lumi_cms[x_cms < x][-1] if any(x_cms < x) else 1 for x in xx])
+
+            ax3.plot(xx, yy_lumi_atlas/yy_lumi_cms, color="black", marker=None,
+                linestyle='--', zorder=0)
+
+            ratio = round(y_lumi_atlas[-1] / y_lumi_cms[-1],3)
+
+            ax3.plot(np.array([xMin, xMax]), np.array([1.0, 1.0]), color="black", linestyle="--", linewidth=1)
+            
+            ax3.text(0.4, 0.6 if ratio<1 else 0.2, "Integrated L ratio: "+str(ratio), verticalalignment='bottom', transform=ax3.transAxes)
+
+            ax3.set_ylim([0.81,1.19])
+            ax3.set_xlim([xMin, xMax])
+            ax3.set_xticks(xTicks)
+
+            ax3.yaxis.set_label_coords(-0.12, 0.5)
+
         # align y labels
         ax1.yaxis.set_label_coords(-0.12, 0.5)
         ax2.yaxis.set_label_coords(-0.12, 0.5)
+
     else:
         ax1.set_xlabel("LHC runtime")
 
